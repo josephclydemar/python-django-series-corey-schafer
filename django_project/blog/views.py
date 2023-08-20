5b0bb44d3bb0c70b796032f4208a03b1cd3e8318
Joseph Clyde Mar <clydemar234@gmail.com>
2023-08-20 16:43:14 +0800

    Put the home.html and about.html templates in views.py to be rendered to the browser.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    # return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
 
 def contacts(request):
     return HttpResponse('<h3>Blog Contacts HAHA..</h3>')
